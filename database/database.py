93abfbb0f4937206b09cda121a8069b03ee14561
iamutkarshusv <109755244+iamutkarshusv@users.noreply.github.com>
2023-05-23 14:59:13 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,36 +1,33 @@
-#(©)CodeXBotz
+# (©)CodeXBotz
 
 
+from motor.motor_asyncio import AsyncIOMotorClient
 
+from config import DB_NAME, DB_URI
 
-import pymongo, os
-from config import DB_URI, DB_NAME
-
-
-dbclient = pymongo.MongoClient(DB_URI)
+dbclient = AsyncIOMotorClient(DB_URI)
 database = dbclient[DB_NAME]
 
 
-user_data = database['users']
+user_data = database["users"]
 
 
+async def present_user(user_id: int):
+    return bool(await user_data.find_one({"_id": user_id}))
 
-async def present_user(user_id : int):
-    found = user_data.find_one({'_id': user_id})
-    return bool(found)
 
 async def add_user(user_id: int):
-    user_data.insert_one({'_id': user_id})
-    return
+    await user_data.insert_one({"_id": user_id})
+
 
 async def full_userbase():
-    user_docs = user_data.find()
+    user_docs = await user_data.find()
     user_ids = []
     for doc in user_docs:
-        user_ids.append(doc['_id'])
-        
+        user_ids.append(doc["_id"])
+
     return user_ids
 
+
 async def del_user(user_id: int):
-    user_data.delete_one({'_id': user_id})
-    return
+    await user_data.delete_one({"_id": user_id})
